ee91745c15b1b8ec171ec12a93b06dd499096b77
AdusparxAdith <adithdinesh97@gmail.com>
2019-07-04 11:34:59 +0530

    Readying for Heroku

--- index.js
+++ index.js
@@ -5,21 +5,7 @@ const dbURL = config.get("mongoURI");
 const cors = require("cors");
 const app = express();
 const db = require("./config/db");
-
-const port = 5000;
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-app.use(cors());
-app.use("/api/orders", require("./routes/api/orders"));
-app.use("/api/update", require("./routes/api/update"));
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+const path = require("path");
 
 db.connect(dbURL, function(err) {
   if (err) {
@@ -30,6 +16,27 @@ db.connect(dbURL, function(err) {
   }
 });
 
-app.listen(port, function() {
-  console.log("Listening on port 5000...");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api/orders", require("./routes/api/orders"));
+app.use("/api/update", require("./routes/api/update"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}...`);
 });
